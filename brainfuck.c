15ed7dffe35bd18e4e2485073cd2400ec4bd8a08
Marcos Minond <minond.marcos@gmail.com>
2018-01-15 17:48:01 -0700

    cleaning up some codegen

--- brainfuck.c
+++ brainfuck.c
@@ -21,6 +21,12 @@ void emit_loop_end() {}
 int main(void) {
   char* code = ",.";
 
+  llvm_declare("calloc", i8ptr, 2, i32, i32);
+  llvm_declare("free", Void, 1, i8ptr);
+
+  llvm_declare("putchar", i32, 1, i32);
+  llvm_declare("getchar", i32, 0);
+
   llvm_main_start();
 
   for (int i = 0; code[i] != '\0'; i++) {
--- emitter.c
+++ emitter.c
@@ -5,18 +5,20 @@
   const char* label = #label; \
   const char* label ## ptr = #label "*";
 
+#define def_llvm_labeled_type(label, value) \
+  const char* label = #value; \
+  const char* label ## ptr = #value "*";
+
 def_llvm_type(i8);
 def_llvm_type(i32);
 def_llvm_type(i64);
+def_llvm_labeled_type(Void, void);
 
 void llvm_ret(const char* ret, const char* val) {
   printf("  ret %s %s;\n", ret, val);
 }
 
-void llvm_fn(const char* kind, const char* name, const char* ret, int arity, ...) {
-  va_list args;
-  va_start(args, arity);
-
+void llvm_fn(const char* kind, const char* name, const char* ret, int arity, va_list args) {
   printf("%s %s @%s(", kind, ret, name);
 
   for (int i = 0; i < arity; i++) {
@@ -27,27 +29,34 @@ void llvm_fn(const char* kind, const char* name, const char* ret, int arity, ...
     printf("%s", va_arg(args, char*));
   }
 
-  va_end(args);
-  printf(")\n");
+  printf(")");
 }
 
-void llvm_declare_start(const char* name, const char* ret, int arity, ...) {
+void llvm_declare(const char* name, const char* ret, int arity, ...) {
   va_list args;
   va_start(args, arity);
-  printf("; Function definition start: %s\n", name);
-  llvm_fn("define", name, ret, arity, args);
+  llvm_fn("declare", name, ret, arity, args);
   va_end(args);
-  printf("{\n");
+  putchar('\n');
 }
 
-void llvm_declare_close(const char* name) {
+void llvm_define_start(const char* name, const char* ret, int arity, ...) {
+  va_list args;
+  va_start(args, arity);
+  printf("\n; Function definition start: %s\n", name);
+  llvm_fn("define", name, ret, arity, args);
+  va_end(args);
+  printf(" {\n");
+}
+
+void llvm_define_close(const char* name) {
   printf("}\n; Function definition end: %s\n", name);
 }
 
 void llvm_main_start() {
-  llvm_declare_start("main", i32, 0);
+  llvm_define_start("main", i32, 0);
 }
 
 void llvm_main_close() {
-  llvm_declare_close("main");
+  llvm_define_close("main");
 }
